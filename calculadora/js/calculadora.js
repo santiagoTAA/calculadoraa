b1f2f56b3909cb9e3fb76770263bde2ee1b5df93
santiagoTAA <123841371+santiagoTAA@users.noreply.github.com>
2023-01-28 16:56:34 -0500

    primer push calculadora

---
+++ calculadora/js/calculadora.js
@@ -0,0 +1,31 @@
+function operaciones(){
+    let botonSuma = document.getElementsByClassId("suma")
+    botonSuma.addEventListener("click",suma)
+    let botonResta = document.getElementsByClassId("resta")
+    botonResta.addEventListener("click",resta)
+    let botonDivision = document.getElementsByClassId("division")
+    botonDivision.addEventListener("click",division)
+    let botonMultiplicacion = document.getElementsByClassId("multi")
+    botonMultiplicacion.addEventListener("click",multiplicacion)
+   
+    muestraPanatalla.display
+}
+let muestraPanatalla = document.getElementById("resultado")
+muestraPanatalla.value= 23
+
+function pantalla(){
+ 
+}
+
+function suma(){
+
+}
+function resta(){
+}
+function division(){
+}
+function multiplicacion(){  
+}
+
+let aa =document.getElementById("aa")
+aa.innerHTML= '<input value="" placeholder="hola" > '
